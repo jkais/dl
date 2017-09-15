c9c96633ffb6b867eb5bcebdb680bbbc988ba292
Jan Kaiser <jan.kaiser@blau.de>
2017-09-15 16:42:18 +0200

    added some kind of image loading script

---
+++ 1/image.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+my_image = "my_image.jpg"   # change this to the name of your image file
+
+# We preprocess the image to fit your algorithm.
+fname = "images/" + my_image
+image = np.array(ndimage.imread(fname, flatten=False))
+my_image = scipy.misc.imresize(image, size=(num_px,num_px)).reshape((1, num_px*num_px*3)).T
+my_predicted_image = predict(d["w"], d["b"], my_image)
