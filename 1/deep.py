1471df03b3592ba0f11431c7fc556adec52d62fc
jkais <kaiser.jan@gmail.com>
2017-11-15 11:05:29 +0100

    linting deep.py

--- 1/deep.py
+++ 1/deep.py
@@ -10,7 +10,7 @@ print("Number of training results: " + str(train_y.shape[0]))
 # flatten data
 train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T
 # normalize values between 0 and 1
-train_x = train_x_flatten/255
+train_x = train_x_flatten / 255
 
 layers_dims = [12288, 20, 7, 5, 1]
 
@@ -23,37 +23,32 @@ def initialize_parameters_deep(layers_dims):
     L = len(layers_dims)            # number of layers in the network
 
     for l in range(1, L):
-        parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l-1]) * 0.01
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l - 1]) * 0.01
         parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
 
-        assert(parameters['W' + str(l)].shape == (layers_dims[l], layers_dims[l-1]))
+        assert(parameters['W' + str(l)].shape == (layers_dims[l], layers_dims[l - 1]))
         assert(parameters['b' + str(l)].shape == (layers_dims[l], 1))
 
     return parameters
 
 
 def sigmoid(Z):
-    A = 1/(1+np.exp(-Z))
+    A = 1 / (1 + np.exp(-Z))
     cache = Z
-
     return A, cache
 
 
 def relu(Z):
-    A = np.maximum(0,Z)
-
+    A = np.maximum(0, Z)
     assert(A.shape == Z.shape)
-
     cache = Z
     return A, cache
 
 
 def linear_forward(A, W, b):
     Z = np.dot(W, A) + b
-
     assert(Z.shape == (W.shape[0], A.shape[1]))
     cache = (A, W, b)
-
     return Z, cache
 
 
@@ -138,7 +133,7 @@ def compute_cost(AL, Y):
 
     # Compute loss from aL and y.
     logprobs = np.multiply(np.log(AL), Y) + np.multiply(np.log(1 - AL), (1 - Y))
-    cost = - np.sum(logprobs)/m
+    cost = - np.sum(logprobs) / m
 
     cost = np.squeeze(cost)      # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
     assert(cost.shape == ())
@@ -162,8 +157,8 @@ def linear_backward(dZ, cache):
     A_prev, W, b = cache
     m = A_prev.shape[1]
 
-    dW = np.dot(dZ, A_prev.T)/m
-    db = 1./m * np.sum(dZ, axis=1, keepdims=True)
+    dW = np.dot(dZ, A_prev.T) / m
+    db = 1. / m * np.sum(dZ, axis=1, keepdims=True)
     dA_prev = np.dot(cache[1].T, dZ)
 
     assert (dA_prev.shape == A_prev.shape)
@@ -175,8 +170,8 @@ def linear_backward(dZ, cache):
 
 def sigmoid_backward(dA, cache):
     Z = cache
-    s = 1/(1+np.exp(-Z))
-    dZ = dA * s * (1-s)
+    s = 1 / (1 + np.exp(-Z))
+    dZ = dA * s * (1 - s)
     assert (dZ.shape == Z.shape)
     return dZ
 
@@ -235,17 +230,16 @@ def L_model_backward(AL, Y, caches):
     """
     grads = {}
     L = len(caches)  # the number of layers
-    m = AL.shape[1]
     Y = Y.reshape(AL.shape)  # after this line, Y is the same shape as AL
 
     # Initializing the backpropagation
     dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
 
     # Lth layer (SIGMOID -> LINEAR) gradients. Inputs: "AL, Y, caches". Outputs: "grads["dAL"], grads["dWL"], grads["dbL"]
-    current_cache = caches[L-1]
+    current_cache = caches[L - 1]
     grads["dA" + str(L)], grads["dW" + str(L)], grads["db" + str(L)] = linear_activation_backward(dAL, current_cache, activation="sigmoid")
 
-    for l in reversed(range(L-1)):
+    for l in reversed(range(L - 1)):
         # lth layer: (RELU -> LINEAR) gradients.
         current_cache = caches[l]
         dA_prev_temp, dW_temp, db_temp = linear_activation_backward(grads["dA" + str(l + 2)], current_cache, activation="relu")
@@ -261,21 +255,21 @@ def update_parameters(parameters, grads, learning_rate):
     Update parameters using gradient descent
 
     Arguments:
-    parameters -- python dictionary containing your parameters 
+    parameters -- python dictionary containing your parameters
     grads -- python dictionary containing your gradients, output of L_model_backward
 
     Returns:
-    parameters -- python dictionary containing your updated parameters 
-                  parameters["W" + str(l)] = ... 
+    parameters -- python dictionary containing your updated parameters
+                  parameters["W" + str(l)] = ...
                   parameters["b" + str(l)] = ...
     """
 
-    L = len(parameters) // 2 # number of layers in the neural network
+    L = len(parameters) // 2  # number of layers in the neural network
 
     # Update rule for each parameter. Use a for loop.
     for l in range(L):
-        parameters["W" + str(l+1)] = parameters["W" + str(l+1)] - learning_rate * grads["dW" + str(l+1)]
-        parameters["b" + str(l+1)] = parameters["b" + str(l+1)] - learning_rate * grads["db" + str(l+1)]
+        parameters["W" + str(l + 1)] = parameters["W" + str(l + 1)] - learning_rate * grads["dW" + str(l + 1)]
+        parameters["b" + str(l + 1)] = parameters["b" + str(l + 1)] - learning_rate * grads["db" + str(l + 1)]
 
     return parameters
 
@@ -292,9 +286,10 @@ def L_layer_model(X, Y, layers_dims, learning_rate=0.0075, num_iterations=3000, 
         parameters = update_parameters(parameters, grads, learning_rate)
 
         if print_cost and i % 100 == 0:
-            print ("Cost after iteration %i: %f" %(i, cost))
+            print ("Cost after iteration %i: %f" % (i, cost))
             costs.append(cost)
 
     return parameters
 
-L_layer_model(train_x, train_y, layers_dims, print_cost=True)
+
+parameters = L_layer_model(train_x, train_y, layers_dims, print_cost=True)
