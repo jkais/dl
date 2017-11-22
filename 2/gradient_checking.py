c1054c622c12d9695d812d28865745c49fd4d0fa
Jan Kaiser <jan.kaiser@blau.de>
2017-11-22 17:08:39 +0100

    working on gradient checking...

--- 2/gradient_checking.py
+++ 2/gradient_checking.py
@@ -3,31 +3,82 @@ import h5py
 import numpy as np
 from helpers import initialize_parameters_deep, L_model_forward, L_model_backward, update_parameters, compute_cost, predict
 
+def dictionary_to_vector(parameters):
+    """
+    Roll all our parameters dictionary into a single vector satisfying our specific required shape.
+    """
+    keys = []
+    count = 0
+    for key in ["W1", "b1", "W2", "b2", "W3", "b3"]:
+
+        # flatten parameter
+        new_vector = np.reshape(parameters[key], (-1,1))
+        keys = keys + [key]*new_vector.shape[0]
+
+        if count == 0:
+            theta = new_vector
+        else:
+            theta = np.concatenate((theta, new_vector), axis=0)
+        count = count + 1
+
+    return theta, keys
+
+
+def vector_to_dictionary(theta):
+    """
+    Unroll all our parameters dictionary from a single vector satisfying our specific required shape.
+    """
+    parameters = {}
+    parameters["W1"] = theta[:20].reshape((5,4))
+    parameters["b1"] = theta[20:25].reshape((5,1))
+    parameters["W2"] = theta[25:40].reshape((3,5))
+    parameters["b2"] = theta[40:43].reshape((3,1))
+    parameters["W3"] = theta[43:46].reshape((1,3))
+    parameters["b3"] = theta[46:47].reshape((1,1))
+
+    return parameters
+
+
+def gradients_to_vector(gradients):
+    """
+    Roll all our gradients dictionary into a single vector satisfying our specific required shape.
+    """
+
+    count = 0
+    for key in ["dW1", "db1", "dW2", "db2", "dW3", "db3"]:
+        # flatten parameter
+        new_vector = np.reshape(gradients[key], (-1,1))
+
+        if count == 0:
+            theta = new_vector
+        else:
+            theta = np.concatenate((theta, new_vector), axis=0)
+        count = count + 1
+
+    return theta
+
 
 def check_gradients(parameters, grads, X, Y, epsilon=1e-7):
-    # TODO: vector_to_dictionary, dictionary_to_vector functions
-
-    # TODO: vectorize parameters
-    parameters_values = parameters
+    parameters_values, _ = dictionary_to_vector(parameters)
     # TODO: vectorize grads
-    grads_values = grads
+    grads_values = gradients_to_vector(grads)
 
     num_params = parameters_values.shape[0]
     J_plus = np.zeros((num_params, 1))
     J_minus = np.zeros((num_params, 1))
     gradapprox = np.zeros((num_params, 1))
 
-    for i in (num_params):
+    for i in range(num_params):
         # Compute costs for theta plus epsilon
         thetaplus = np.copy(parameters_values)
         thetaplus[0][i] = thetaplus[0][i] + epsilon
-        new_AL, _ = L_model_forward(X, thetaplus)
+        new_AL, _ = L_model_forward(X, vector_to_dictionary(thetaplus))
         J_plus[i] = compute_cost(new_AL, Y)
 
         # Compute costs for theta minus epsilon
         thetaminus = np.copy(parameters_values)
         thetaminus[0][i] = thetaminus[0][i] - epsilon
-        new_AL, _ = L_model_forward(X, thetaminus)
+        new_AL, _ = L_model_forward(X, vector_to_dictionary(thetaminus))
         J_minus[i] = compute_cost(new_AL, Y)
 
         gradapprox[i] = (J_plus[i] - J_minus[i]) / (epsilon * 2.)
